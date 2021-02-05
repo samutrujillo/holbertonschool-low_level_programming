0db4ff8940875d4873614efc45510dd6ff31e59c
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-02-04 19:13:58 -0500

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - numbers of base 10
+ *
+ *Return: (0) Always
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+
+	}
+	putchar('\n');
+	return (0);
+}
