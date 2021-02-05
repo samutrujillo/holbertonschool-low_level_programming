420ee8cdb3766f950bf4333663910ab961624230
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-02-04 19:20:31 -0500

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main -  print hexadecimal
+ *
+ *Return: (0) Always
+ */
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (b = 'a'; b <= 'f'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+
+	return (0);
+}
