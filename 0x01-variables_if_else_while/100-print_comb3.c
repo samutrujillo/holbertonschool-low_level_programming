d20d7672c2fde0ac05affd93c229945246d46b2d
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-02-04 19:45:43 -0500

    advanced  variables, if, else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - supercombination
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a <= '8'; a++)
+		for (b = '1'; b <= '9'; b++)
+		{
+			if (!(a >= b))
+			{
+				putchar(a);
+				putchar(b);
+				if (!(a == '8' && b == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	putchar('\n');
+	return (0);
+}
