5e424fe83f58979442a45de6e4ca79f38109dace
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-02-09 18:46:30 -0500

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * jack_bauer - Prints hours.
+ * Description: Prints time of a day
+ * Return: none.
+ */
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
