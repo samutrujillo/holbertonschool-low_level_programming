7490c5eb6f7fba77c393dba7e732e25a8cbfbead
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-02-09 18:29:38 -0500

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _abs - Prints absolute integer.
+ * Description: Prints absolute value of an integer
+ * @n: integer to evaluate
+ * Return: Return absolute int.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
