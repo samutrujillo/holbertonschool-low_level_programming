ae74be513c0f840cf5807c09b40822ef9a325704
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-02-09 18:55:39 -0500

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - Adds two integers.
+ * Description: Concatenate two integers
+ * @a: integer one
+ * @b: integer two
+ * Return: 2 integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
