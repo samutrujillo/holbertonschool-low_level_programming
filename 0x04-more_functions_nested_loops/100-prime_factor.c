91740c4ac7c7e884b3439061bf36d55757aa3835
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-02-11 16:03:29 -0500

    Advanced C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - Prints bigger prime factor
+ * Description: Lok for biggest prime factor of a number
+ * Return: 0.
+ */
+int main(void)
+{
+	long i;
+	long num = 612852475143;
+
+	for (i = sqrt(num); i < num; i++)
+	{
+		if (num % i == 0)
+		{
+			printf("%ld\n", i);
+			break;
+		}
+	}
+	return (0);
+}
