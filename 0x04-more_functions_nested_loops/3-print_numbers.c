f1115a9a4d2bc50137e72b0fbcd9dcd0a2143d6a
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-02-11 13:24:17 -0500

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_numbers - function
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar (i);
+	}
+	_putchar ('\n');
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 /*prototypes*/
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
