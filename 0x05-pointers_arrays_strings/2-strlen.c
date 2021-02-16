69c1c1d86a0d9340c13fa019f3d66d458548f409
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-02-16 10:43:00 -0500

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *_strlen - function
+ *@s: variable
+ *Return: gave i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+	}
+	return (i);
+}
