b94e0bbae39e7ca17f9f1a746ca2d60ff389c84f
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-02-18 22:33:23 -0500

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,10 +1,8 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-//libraries//
 #include <stdio.h>
 
-//prototypes//
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
