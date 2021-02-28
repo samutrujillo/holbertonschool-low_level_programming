6de4be7d25c00b813109bded3769e4c13d4a7816
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-02-27 20:18:35 -0500

    C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: variable
+ * @argv: variable
+ * Return: 0.
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+
+	printf("%i\n", argc - 1);
+	return (0);
+}
