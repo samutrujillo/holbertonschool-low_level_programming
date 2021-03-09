45de436af487e6e23ce484d36f70490c8ba78c86
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-03-08 22:03:47 -0500

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - check the code for Holberton School Student
+ *@name: the name
+ *@age: the age
+ *@owner: the owner
+ */
+
+typedef struct dog
+{
+
+	char *name;
+
+	float age;
+
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
