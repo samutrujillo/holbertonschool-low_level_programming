8b312a2bde673cea44905497dc6a48de8dc20d26
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-03-11 22:16:01 -0500

    Variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,22 +1,21 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 /**
- * sum_them_all - function that returns the sum of all its parameters.
- * @n: variable
- * Return: Sum
+ * sum_them_all - function that returns the sum of all its parameters
+ *@n: variable
+ *Return: dest
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list numbers;
-	unsigned int i;
-	int sum = 0;
+	unsigned int i, sum = 0;
+	va_list arg;
 
 	if (n == 0)
 		return (0);
-	va_start(numbers, n);
-	for (i = 0; i < n; i++)
-		sum += va_arg(numbers, int);
-	va_end(numbers);
 
+	va_start(arg, n);
+
+	for (i = 0; i < n; i++)
+		sum = sum + va_arg(arg, unsigned int);
+	va_end(arg);
 	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,20 +1,29 @@
-ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
 
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * struct chfmt - struct
+ *@f: the variable.
+ *@c: another the variable
+ *return: dest
+ */
+typedef struct chfmt
+{
+	char c;
+	void (*f)(va_list);
+} fmt_t;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-/**
- * struct print - Struct print
- * @type: Format
- * @f: The function associated
- */
-typedef struct print
-{
-	char type;
-        int (*f)();
-} p_all;
-
-#endif
+void p_char(va_list c);
+void p_integer(va_list i);
+void p_float(va_list f);
+void p_string(va_list s);
+#endif /* VARIADIC_FUNCTIONS_H */
