c10a3558ef5f5ae65fc2da9ea0baa996c0542a22
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-03-19 17:03:20 -0500

    Singly linked lists

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+	SECTION .data
+fmt:	 	db "%s", 10, 0
+msg:		db "Hello, Holberton"
+
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov  esi, msg
+	mov  edi, fmt
+	mov  eax, 0
+	call printf
+
+	mov eax, 0
+	ret
