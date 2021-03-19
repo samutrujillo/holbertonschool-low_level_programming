d510dff942ad0dfec32d08308190a8f4b25fce82
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-03-19 11:04:54 -0500

    Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * before - add a text 
+ * Return: void
+ */
+void __attribute__ ((constructor)) before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
