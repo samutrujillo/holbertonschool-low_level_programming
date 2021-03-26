dd675366d31ec0e8ff9edab878feaaf33c71c8a0
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-03-25 23:43:52 -0500

    Bit manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * flip_bits - function that returns the number of bits 
+ * @n: var
+ * @m: var
+ * Return: j
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, j = 0;
+
+	i = sizeof(n) * 8 - 1;
+	while (i >= 0)
+	{
+		i--;
+		if (((n ^ m) >> i) & 1)
+			j++;
+	}
+	return (j);
+}
