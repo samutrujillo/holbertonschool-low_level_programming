7f548cbbcbedb799cffe6e7ce71a522b4938e3d6
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-03-29 23:25:03 -0500

    File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+nes (33 sloc) 791 Bytes
+#include "holberton.h"
+
+/**
+ * read_textfile - function
+ * @filename: var
+ * @letters: var
+ * Return: number of letters read and printed, or 0 if error
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t RDcount, WDcount;
+	char *buffer = NULL;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+	RDcount = read(fd, buffer, letters);
+	if (RDcount == -1)
+		return (0);
+	WDcount = write(STDOUT_FILENO, buffer, RDcount);
+	if (WDcount == -1 || RDcount != WDcount)
+		return (0);
+
+	free(buffer);
+	close(fd);
+	return (WDcount);
+
+}
