7c0478a489abb8a378436c13bf6a0e496f239d2a
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-05-28 20:50:01 -0500

    [<0x1A ~ 3-hash_table_set.c](<C-hash table>)

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function
+ * @ht: Hash table
+ * @key: Key
+ * Return: Pointer to value, NULL if failed
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	hash_node_t *aux;
+
+	if (!ht || !key)
+		return (NULL);
+	i = key_index((const unsigned char *)key, ht->size);
+	if (i >= ht->size)
+		return (NULL);
+
+	aux = ht->array[i];
+	while (aux && strcmp(aux->key, key) != 0)
+		aux = aux->next;
+
+	if (aux && strcmp(aux->key, key) == 0)
+		return (aux->value);
+
+
+	return (NULL);
+}
